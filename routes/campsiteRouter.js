fc01b46579ca9f2c1f7805eb410c0f630e9b5592
Jesse Wey <jtwey08@gmail.com>
2023-06-03 12:28:56 -0700

    Week 1: workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites')
 })
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+})
+.get((req, res) => {
+    res.end(`Sending details for campsite: ${req.params.campsiteId}`)
+})
+.post((req, res) => {
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+})
+.put((req, res) => {
+    res.end(`Updating the campsite: ${req.params.campsiteId}
+    Will update the campsite: ${req.body.name} with description: ${req.body.description}`)
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`)
+})
+
 module.exports = campsiteRouter
